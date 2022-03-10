72f95d7dd0038044f633e0c35c9f1530393c4537
Can Surer <cansurer@outlook.com>
2022-03-10 15:48:28 +0300

    Updates

---
+++ precache-manifest.1bcab974cc0d391e3b04c911159e74fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf6a67c2ad1fa2243f7ed424a6f1355",
+    "url": "/To-Do-List-React-App/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/To-Do-List-React-App/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "73408dd5da69b87ffb4b",
+    "url": "/To-Do-List-React-App/static/js/main.36c18deb.chunk.js"
+  },
+  {
+    "revision": "d2e4d8eee402e0f42bed",
+    "url": "/To-Do-List-React-App/static/js/runtime-main.1bac4969.js"
+  }
+]);
